92f722dfd7c355e1f0db66cfc6dd15d5517fc955
Fabián Henry <fhenrynk@gmail.com>
2023-05-31 12:40:13 -0400

    fix: dumb model name changed

--- server/models/TrackUserRating.js
+++ server/models/TrackUserRating.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize')
 const sequelize = require('../db')
 
-const TrackUserRating = sequelize.define('AudioFile', {
+const TrackUserRating = sequelize.define('TrackUserRating', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
