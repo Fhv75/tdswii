0bd7b5ee73d69c65c00e102a4add1f220bef4be1
ismapuntocom <115047902+ismapuntocom@users.noreply.github.com>
2023-06-02 13:12:19 -0400

    Revert "Merge branch 'develop' into feature/mostrarEstadisticasPista"
    This reverts commit ba42a34314e10781a4a088e00ccb48298fe01896, reversing
    changes made to 3de9f38f4c69ad05a3b2f427bf737cd28e5e39e1.

--- client/src/Pages/UserProfile/UserProfile.jsx
+++ client/src/Pages/UserProfile/UserProfile.jsx
@@ -22,6 +22,7 @@ export default function UserProfile() {
                 username : username
             }, 
           )  
+          console.log(response.data)
           setTracksData(response.data.AudioFiles)
         }
         catch(error){
--- server/routes/audio.js
+++ server/routes/audio.js
@@ -5,11 +5,9 @@ const { upload, uploadAudioFile, getUserTracks, getTrackTags, getStatistics } = 
 
 router.post('/upload/:trackData', upload.single('audioFile'), uploadAudioFile)
 router.post('/getStatistics', getStatistics)
-const { upload, uploadAudioFile, getUserTracks, getTrackTags, rateTrack, getUserTrackRating } = require('../controllers/audioController')
 
 router.post('/upload/:trackData', upload.single('audioFile'), uploadAudioFile)
 router.post('/getUserTracks', getUserTracks )
 router.post('/getTrackTags', getTrackTags )
-router.post('/rateTrack', rateTrack)
-router.post('/getUserRating', getUserTrackRating)
+
 module.exports = router
