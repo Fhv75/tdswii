c051b6d716510d8ba19e5893fabbe0b01be18afa
WaldoPalacios <waldo.palacios.fuenzalida@alumnos.uta.cl>
2023-06-06 16:12:59 -0400

    login + register messages

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,98 +1,122 @@
-import React, { useState } from 'react'
-import { Button, Container, Form } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import styles from './login.module.css'
+import React, { useState } from 'react';
+import { Button, Container, Form, Spinner } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import styles from './login.module.css';
+import { useToast } from '@chakra-ui/react';
 
 function Login() {
+  const [data, setData] = useState({ email: '', password: '' });
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const toast = useToast();
 
-    const [data, setData] = useState({email: "", password: ""})
-    const navigate = useNavigate()
+  function inputHandler(e) {
+    const { id, value } = e.target;
+    setData((prevData) => ({ ...prevData, [id]: value }));
+  }
 
-    function inputHandler(e) {
-        const { id, value } = e.target
-        setData(prevData => ({ ...prevData, [id]: value}))
+  async function submitHandler(e) {
+    e.preventDefault();
+    await loginUser();
+  }
+
+  async function loginUser() {
+    setIsLoading(true);
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = true;
+    try {
+      const response = await axios.post('http://localhost:5000/users/login', {
+        correo: data.email,
+        password: data.password,
+      });
+      localStorage.setItem('token', response.data.token);
+      localStorage.setItem('username', response.data.username);
+      navigate(`../user/${response.data.username}`);
+      toast({
+        title: 'Inicio de sesión exitoso',
+        description: '¡Bienvenido/a!',
+        status: 'success',
+        colorScheme: 'green',
+        duration: 3000,
+        isClosable: true,
+      });
+    } catch (error) {
+      toast({
+        title: 'Los datos ingresados son incorrectos.',
+        status: 'success',
+        colorScheme: 'red',
+        duration: 3000,
+        isClosable: true,
+      });
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
-    async function submitHandler(e) {
-        e.preventDefault()
-        await loginUser()
-    }
+  return (
+    <Container fluid className={styles['login-container'] + ' p-5 bg-white mt-5'}>
+      <div className="text-center mb-4">
+        <h1 className="px-sm-5">Iniciar Sesión</h1>
+        <hr />
+      </div>
 
-    async function loginUser() {
-        axios.defaults.headers.post["Access-Control-Allow-Origin"] = true
-        try {
-            const response = await axios.post(
-                'http://localhost:5000/users/login',
-                { 
-                    correo: data.email, 
-                    password: data.password 
-                }, 
-            )
-            localStorage.setItem('token', response.data.token)
-            localStorage.setItem('username', response.data.username)
-            navigate(`../user/${response.data.username}`)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    
-    return (
-        <Container fluid className={styles["login-container"] + " p-5 bg-white mt-5"}>
-            <div className="text-center mb-4">
-                <h1 className="px-sm-5">Iniciar Sesión</h1>
-                <hr />
-            </div>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="pt-3 mb-4" controlId="email">
+          <Form.Label>Correo Electrónico</Form.Label>
+          <Form.Control
+            onChange={inputHandler}
+            type="email"
+            placeholder="Ingresa tu Correo"
+            value={data.email}
+          />
+        </Form.Group>
 
-            <Form onSubmit={submitHandler}>
-                <Form.Group className="pt-3 mb-4" controlId="email">
-                    <Form.Label>Correo Electrónico</Form.Label>
-                    <Form.Control 
-                        onChange={inputHandler}  
-                        type="email" 
-                        placeholder="Ingresa tu Correo" 
-                        value={data.email}
-                    />
-                </Form.Group>
+        <Form.Group className="mb-5" controlId="password">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Contraseña"
+            onChange={inputHandler}
+            value={data.password}
+          />
+        </Form.Group>
 
-                <Form.Group className="mb-5" controlId="password">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Contraseña"
-                        onChange={inputHandler}
-                        value={data.password}
-                    />
-                </Form.Group>
+        <Button className="mb-3 w-100" variant="primary" type="submit" disabled={isLoading}>
+          {isLoading ? (
+            <>
+              <Spinner animation="border" size="sm" role="status" /> Cargando...
+            </>
+          ) : (
+            'Ingresar'
+          )}
+        </Button>
+      </Form>
 
-                <Button className="mb-3 w-100" variant="primary" type="submit">Ingresar</Button>
-            </Form>
+      <div className="text-center text-secondary">
+        <p>O</p>
 
-            <div className="text-center text-secondary">
-                <p>O</p>
+        <Button
+          className="mb-3 w-100"
+          variant="dark"
+          type="button"
+          onClick={() => navigate('../register')}
+        >
+          Registrate
+        </Button>
 
-                <Button 
-                    className="mb-3 w-100" 
-                    variant="dark" 
-                    type="button" 
-                    onClick={() => navigate("../register")}
-                >
-                    Registrate
-                </Button>
+        <hr />
 
-                <hr />
-
-                <Button 
-                    variant="link" 
-                    type="button" 
-                    onClick={() => navigate("../password-recovery")}
-                >
-                    ¿Olvidaste tu Contraseña?
-                </Button>
-
-            </div>
-        </Container>
-    )
+        <Button
+          variant="link"
+          type="button"
+          onClick={() => navigate('../password-recovery')}
+        >
+          ¿Olvidaste tu Contraseña?
+        </Button>
+      </div>
+    </Container>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,128 +1,146 @@
-import React, { useState } from 'react'
-import { Button, Form, Container } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import styles from './register.module.css'
+import React, { useState } from 'react';
+import { Button, Form, Container, Spinner } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import styles from './register.module.css';
+import { useToast } from '@chakra-ui/react';
 
 function Register() {
+  const toast = useToast();
 
-    const [data, setData] = useState({email: "", username: "", password: "", confirmPw: ""})
-    const navigate = useNavigate()
+  const [data, setData] = useState({
+    email: '',
+    username: '',
+    password: '',
+    confirmPw: '',
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-    function inputHandler(e) {
-        const { id, value } = e.target
-        setData(prevData => ({ ...prevData, [id]: value}))
+  function inputHandler(e) {
+    const { id, value } = e.target;
+    setData((prevData) => ({ ...prevData, [id]: value }));
+  }
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    await registerUser();
+  }
+
+  async function registerUser() {
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = true;
+    try {
+      setIsLoading(true); // Activar el estado de carga
+
+      const response = await axios.post('http://localhost:5000/users/register', {
+        correo: data.email,
+        username: data.username,
+        password: data.password,
+      });
+      localStorage.setItem('token', response.data.token);
+      localStorage.setItem('username', response.data.username);
+      navigate(`/complete-profile`);
+      toast({
+        title: 'Registro exitoso.',
+        status: 'success',
+        colorScheme: 'green',
+        duration: 3000,
+        isClosable: true,
+      });
+    } catch (error) {
+      toast({
+        title: 'Registro no exitoso.',
+        status: 'success',
+        colorScheme: 'red',
+        duration: 3000,
+        isClosable: true,
+      });
+      console.log(error);
+    } finally {
+      setIsLoading(false); // Desactivar el estado de carga
     }
+  }
 
-    async function submitHandler(e) {
-        e.preventDefault()
-        await registerUser()
-    }
+  return (
+    <Container fluid className={styles['register-container'] + ' p-5 bg-white mt-5'}>
+      <div className="text-center mb-4">
+        <h1 className="px-sm-5">Registrarse</h1>
+        <hr />
+      </div>
 
-    async function registerUser() {
-        axios.defaults.headers.post["Access-Control-Allow-Origin"] = true
-        try {
-            
-            const response = await axios.post(
-                'http://localhost:5000/users/register',
-                { 
-                    correo: data.email, 
-                    username: data.username,
-                    password: data.password 
-                }, 
-            )
-            localStorage.setItem('token', response.data.token)
-            localStorage.setItem('username', response.data.username)
-            navigate(`/complete-profile`)
-        } catch (error) {
-            console.log(error)
-        }
-    }
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="pt-3 mb-4" controlId="username">
+          <Form.Label>Nombre de Usuario</Form.Label>
+          <Form.Control
+            type="username"
+            placeholder="Ingresa tu Nombre de Usuario"
+            onChange={inputHandler}
+            value={data.username}
+            required
+          />
+        </Form.Group>
 
-    return (
-        <Container fluid className={styles["register-container"] + " p-5 bg-white mt-5"}>
-            <div className="text-center mb-4">
-                <h1 className="px-sm-5">Registrarse</h1>
-                <hr />
-            </div>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Correo Electrónico</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Ingresa tu Correo"
+            onChange={inputHandler}
+            value={data.email}
+            required
+          />
+        </Form.Group>
 
-            <Form onSubmit={submitHandler}>
-                <Form.Group className="pt-3 mb-4" controlId="email">
-                    <Form.Label>Nombre de Usuario</Form.Label>
-                    <Form.Control
-                        type="username"
-                        placeholder="Ingresa tu Nombre de Usuario"
-                        onChange={inputHandler}
-                        value={data.username}
-                        required
-                    />
-                </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Contraseña"
+            onChange={inputHandler}
+            value={data.password}
+            required
+          />
+        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="email">
-                    <Form.Label>Correo Electrónico</Form.Label>
-                    <Form.Control
-                        type="email"
-                        placeholder="Ingresa tu Correo"
-                        onChange={inputHandler}
-                        value={data.email}
-                        required
-                    />
-                </Form.Group>
+        <Form.Group className="mb-5" controlId="confirmPw">
+          <Form.Label>Confirmar Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Contraseña"
+            onChange={inputHandler}
+            value={data.confirmPw}
+            required
+          />
+          {data.password !== data.confirmPw && (
+            <p className="text-danger">Las contraseñas no coinciden</p>
+          )}
+        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Contraseña"
-                        onChange={inputHandler}
-                        value={data.password}
-                        required
-                    />
-                </Form.Group>
+        {data.password !== data.confirmPw ? (
+          <Button className="mb-3 w-100" variant="primary" type="submit" disabled>
+            {isLoading ? <Spinner animation="border" size="sm" /> : 'Registrarse'}
+          </Button>
+        ) : (
+          <Button className="mb-3 w-100" variant="primary" type="submit" disabled={isLoading}>
+            {isLoading ? <Spinner animation="border" size="sm" /> : 'Registrarse'}
+          </Button>
+        )}
+      </Form>
 
-                <Form.Group className="mb-5" controlId="confirmPw">
-                    <Form.Label>Confirmar Contraseña</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Contraseña"
-                        onChange={inputHandler}
-                        value={data.confirmPw}
-                        required
-                    />
-                    {
-                        data.password !== data.confirmPw &&
-                        <p className="text-danger">
-                            Las contraseñas no coinciden
-                        </p>
-                    }
-                </Form.Group>
+      <div className="text-center text-secondary">
+        <p>O</p>
 
-                {
-                    data.password !== data.confirmPw ?
-                        <Button className="mb-3 w-100" variant="primary" type="submit" disabled>
-                            Registrarse
-                        </Button>
-                    :
-                        <Button className="mb-3 w-100" variant="primary" type="submit">
-                            Registrarse
-                        </Button>
-                }
-            </Form>
-
-            <div className="text-center text-secondary">
-                <p>O</p>
-
-                <Button
-                    className="mb-3 w-100"
-                    variant="dark"
-                    type="button"
-                    onClick={() => navigate("../login")}
-                >
-                    Inicia Sesión
-                </Button>
-            </div>
-        </Container>
-    )
+        <Button
+          className="mb-3 w-100"
+          variant="dark"
+          type="button"
+          onClick={() => navigate('../login')}
+        >
+          Inicia Sesión
+        </Button>
+      </div>
+    </Container>
+  );
 }
 
-export default Register
+export default Register;
