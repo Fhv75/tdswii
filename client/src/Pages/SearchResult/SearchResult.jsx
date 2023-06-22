b553f6ca2245404aec57ac3771b97629f982ca8e
ismapuntocom <115047902+ismapuntocom@users.noreply.github.com>
2023-06-22 19:57:27 -0400

    feat- SearchResults-Styles

--- client/src/Pages/SearchResult/SearchResult.jsx
+++ client/src/Pages/SearchResult/SearchResult.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import "./SearchResult.css";
 import axios from "axios";
 
 const SearchResult = () => {
@@ -8,6 +9,7 @@ const SearchResult = () => {
   const [trackResults, setTrackResults] = useState([]);
   const [userResults, setUserResults] = useState([]);
   const [tagResults, setTagResults] = useState([]);
+  const [activeTab, setActiveTab] = useState("tracks");
   const [showNoResults, setShowNoResults] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -16,9 +18,12 @@ const SearchResult = () => {
       setIsLoading(true);
 
       try {
-        const trackResponse = await axios.post("http://localhost:5000/audio/searchTracks", {
-          searchTerm: searchTerm,
-        });
+        const trackResponse = await axios.post(
+          "http://localhost:5000/audio/searchTracks",
+          {
+            searchTerm: searchTerm,
+          }
+        );
 
         setTrackResults(trackResponse.data);
       } catch (error) {
@@ -28,9 +33,12 @@ const SearchResult = () => {
 
     const fetchUserResults = async () => {
       try {
-        const userResponse = await axios.post("http://localhost:5000/users/searchUsers", {
-          searchTerm: searchTerm,
-        });
+        const userResponse = await axios.post(
+          "http://localhost:5000/users/searchUsers",
+          {
+            searchTerm: searchTerm,
+          }
+        );
 
         setUserResults(userResponse.data);
       } catch (error) {
@@ -40,10 +48,12 @@ const SearchResult = () => {
 
     const fetchTagResults = async () => {
       try {
-        const tagResponse = await axios.post("http://localhost:5000/audio/searchTags", {
-                searchTerm: searchTerm,
-            
-        });
+        const tagResponse = await axios.post(
+          "http://localhost:5000/audio/searchTags",
+          {
+            searchTerm: searchTerm,
+          }
+        );
 
         setTagResults(tagResponse.data);
       } catch (error) {
@@ -59,53 +69,93 @@ const SearchResult = () => {
   }, [searchTerm]);
 
   useEffect(() => {
-    setShowNoResults(trackResults.length === 0 && userResults.length === 0 && tagResults.length === 0);
+    setShowNoResults(
+      trackResults.length === 0 &&
+        userResults.length === 0 &&
+        tagResults.length === 0
+    );
   }, [trackResults, userResults, tagResults]);
 
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
-    <div>
-      <h1>Resultados para {searchTerm}</h1>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {trackResults.length > 0 && (
-            <>
-              <h2>Pistas:</h2>
-              <ul>
-                {trackResults.map((result) => (
-                  <li key={result.id}>{result.titulo}</li>
-                ))}
-              </ul>
-            </>
-          )}
-          {userResults.length > 0 && (
-            <>
-              <h2>Usuarios:</h2>
-              <ul>
-                {userResults.map((result) => (
-                  <li key={result.id}>
-                      <Link to={`/user/${result.username}`}>{result.username}</Link>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-          {tagResults.length > 0 && (
-            <>
-              <h2>Géneros:</h2>
-              <ul>
-                {tagResults.map((result) => (
-                  <li key={result.id}>{result.TAG}</li>
-                ))}
-              </ul>
-            </>
-          )}
-          {showNoResults && <p>No results found.</p>}
-        </>
-      )}
-    </div>
-  );
+         <div className="search-result-container">
+        <h1 className="result-section-title">Resultados para {searchTerm}</h1>
+        <div className="tab-container">
+          <button
+            className={`tab ${activeTab === "tracks" ? "active" : ""}`}
+            onClick={() => handleTabChange("tracks")}
+          >
+            Pistas
+          </button>
+          <button
+            className={`tab ${activeTab === "users" ? "active" : ""}`}
+            onClick={() => handleTabChange("users")}
+          >
+            Artistas
+          </button>
+          <button
+            className={`tab ${activeTab === "genres" ? "active" : ""}`}
+            onClick={() => handleTabChange("genres")}
+          >
+            Géneros
+          </button>
+        </div>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            {activeTab === "tracks" && (
+              <>
+                {trackResults.length > 0 ? (
+                  <ul className="result-list">
+                    {trackResults.map((result) => (
+                      <li key={result.id}>{result.titulo}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  showNoResults && <p className="no-results">No se encontraron resultados.</p>
+                )}
+              </>
+            )}
+    
+            {activeTab === "users" && (
+              <>
+                {userResults.length > 0 ? (
+                  <ul className="result-list">
+                    {userResults.map((result) => (
+                      <li key={result.id}>
+                        <Link to={`/user/${result.username}`}>
+                          {result.username}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  showNoResults && <p className="no-results">No se encontraron resultados.</p>
+                )}
+              </>
+            )}
+            {activeTab === "genres" && (
+              <>
+                {tagResults.length > 0 ? (
+                  <ul className="result-list">
+                    {tagResults.map((result) => (
+                      <li key={result.id}>{result.TAG}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  showNoResults && <p className="no-results">No se encontraron resultados.</p>
+                )}
+              </>
+            )}
+          </>
+        )}
+      </div>
+    );
+    
 };
 
 export default SearchResult;
