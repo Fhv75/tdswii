3a50ace1387e0d3a61ffe6153724c2bba0763cd6
Fhv7 <fabian.henry.vilaxa@alumnos.uta.cl>
2023-06-26 19:05:33 -0400

    UI

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import { Col, Container, Row } from 'react-bootstrap'
 import SideBar from '../../components/SideBar/SideBar'
 import styles from './dashboard.module.css'
 import UploadProduct from '../UploadProduct/UploadProduct'
+import Ranking from '../Ranking/Ranking'
 
 //TODO: Hacer que este componente solo sea accesible por administradores
 //TODO: Arreglar el breakpoint entre la sidebar y el contenido
@@ -21,7 +22,7 @@ function Dashboard() {
                         screen === "dashboard" ? <h1>Dashboard</h1> : 
                         screen === "uploadProducts" ? <UploadProduct/> : 
                         screen === "trackApproval" ? <h1>Track Approval</h1> :
-                        screen === "ranking" ? <h1>Ranking</h1> : null
+                        screen === "ranking" ? <Ranking /> : null
                     }
                 </Container>
             </Col>
--- client/src/Pages/Ranking/Ranking.jsx
+++ client/src/Pages/Ranking/Ranking.jsx
@@ -1,10 +1,66 @@
 import React from 'react'
+import { Table, Container, Row, Col, OverlayTrigger, Tooltip, Overlay } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faPlay, faComment } from '@fortawesome/free-solid-svg-icons'
 
 function Ranking() {
-    return(
+
+
+    return (
         <div>
-            <h1>Ranking</h1>
-            <table></table>
+            <h1>Ranking de Usuarios</h1>
+            <Container className="bg-white py-4 px-4 mt-4 mt-xl-5">
+                <Row className="mx-5 pe-5">
+                    <Col className="col-10 col-lg-8 col-md-7 col-sm-4">
+                        Ordenar Por:
+                    </Col>
+                    <Col>
+                        <FontAwesomeIcon icon={faPlay} style={{ marginLeft: "10px", height: "32px" }} />
+                    </Col>
+                    <Col>
+                        <FontAwesomeIcon icon={faStar} style={{ height: "30px"}} /> 
+                    </Col>
+                    <Col>
+                        <FontAwesomeIcon icon={faComment} style={{ height: "32px" }} />
+                    </Col>
+                </Row>
+                <hr />
+                <Table striped className="mt-4">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Usuario</th>
+                            <th>Reproducciones Totales</th>
+                            <th>Valoraciones Promedio</th>
+                            <th>Comentarios Totales</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td colSpan={2}>Larry the Bird</td>
+                            <td>@twitter</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Container>
         </div>
     )
 }
+
+export default Ranking
